34b7b924de0dfddde9f6884d011e4cccb080b5e0
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-06-29 15:33:51 +0530

    to display system  problem is added

---
+++ System_Info.py
@@ -0,0 +1,14 @@
+
+import platform
+import os
+import sys
+
+print('============================')
+print('System Information : ')
+print(os.name)
+print(platform.system())
+print(platform.release(),'\n')
+print('============================')
+print('Python Version Installed : ')
+print(sys.version,'\n')
+print('============================')
