576dadd3c7c001ed065a89609a22af8bfce4a49e
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-07-05 10:05:04 +0530

    website opener  demo file is added

---
+++ website_opener.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Just Exploring webbrowser lib
+from webbrowser import open as web_open
+
+
+# Simple function to open given link in webbrowser
+def open_website(link):
+    try:
+        print("Opening link: " + link)
+        web_open(link)
+    except Exception as e:
+        print("Error Occurred:\n {}".format(e))
+
+
+# Just for test
+get_link = input("Link to open: ")
+open_website(get_link)
